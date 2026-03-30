54c16edc7b5a28ca2f43551a3eddacf3e5680cc5
Phạm Trà <phamvantra.dev@gmail.com>
2026-03-30 15:04:49 +0700

    update: main

--- Neo4J/mid-term-2/src/main/java/vn/edu/iuh/fit/app/Main.java
+++ Neo4J/mid-term-2/src/main/java/vn/edu/iuh/fit/app/Main.java
@@ -20,74 +20,51 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("1. Tim Doctor theo ID");
-            System.out.println("2. Thong ke bac si theo chuyen khoa");
-            System.out.println("3. Them Doctor moi");
-            System.out.println("0. Thoat");
-            System.out.print("Chon chuc nang: ");
+        System.out.println("1. Tim Doctor theo ID");
+        System.out.println("2. Thong ke bac si theo chuyen khoa");
+        System.out.println("3. Them Doctor moi");
+        System.out.println("0. Thoat");
+        System.out.print("Chon chuc nang: ");
 
-            int choice = Integer.parseInt(scanner.nextLine());
-
-            try {
-                switch (choice) {
-                    case 1 -> {
-                        System.out.print("NHAP ID DOCTOR: ");
-                        String doctorId = scanner.nextLine();
-                        Doctor doctor = doctorServiceImpl.findDoctorById(doctorId);
-                        if (doctor != null) {
-                            System.out.println("KET QUA: " + doctor);
-                        } else {
-                            System.out.println("KO TIM THAY");
-                        }
-                    }
-                    case 2 -> {
-                        System.out.println("THONG KE SO BAC SI THEO TUNG CHUYEN KHOA CUA MOT KHOA");
-                        System.out.print("NHAP DEPARTMENT NAME: ");
-                        String departmentName = scanner.nextLine();
-                        Map<String, Long> result = doctorServiceImpl.getNoOfDoctorBySpeciality(departmentName);
-                        if (result.isEmpty()) {
-                            System.out.println("KO CO DU LIEU CHO KHOA NAY");
-                        } else {
-                            System.out.println("THONG KE: ");
-                            result.forEach((k, v) -> {
-                                System.out.println(k + ":" + v);
-                            });
-                        }
-                    }
-                    case 3 -> {
-                        System.out.println("THEM DOCTOR MOI");
-                        System.out.println("DoctorId: ");
-                        String id = scanner.nextLine();
-                        System.out.println("Doctor Name: ");
-                        String name = scanner.nextLine();
-                        System.out.println("Doctor Phone: ");
-                        String phone = scanner.nextLine();
-                        System.out.println("DepartmentId: ");
-                        String departmentId = scanner.nextLine();
-                        System.out.println("Speciality: ");
-                        String speciality = scanner.nextLine();
-
-                        Doctor newDoctor = Doctor
-                                .builder()
-                                .doctorId(id)
-                                .name(name)
-                                .phone(phone)
-                                .departmentId(departmentId)
-                                .speciality(speciality)
-                                .build();
-
-                        doctorServiceImpl.addDoctor(newDoctor);
-                        System.out.println("--- THEM DOCTOR THANH CONG! ---");
-                    }
-                    case 0 -> {
-                        System.exit(0);
-                    }
-                    default -> System.out.println("LUA CHON KO HOP LE");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+        try {
+            System.out.println("TIM DOCTOR THEO ID");
+            String doctorId = "DR.017";
+            Doctor doctor = doctorServiceImpl.findDoctorById(doctorId);
+            if (doctor != null) {
+                System.out.println("KET QUA: " + doctor);
+            } else {
+                System.out.println("KO TIM THAY");
             }
+
+
+            System.out.println("THONG KE SO BAC SI THEO TUNG CHUYEN KHOA CUA MOT KHOA");
+            String departmentName = "Internal Medicine";
+            Map<String, Long> result = doctorServiceImpl.getNoOfDoctorBySpeciality(departmentName);
+            if (result.isEmpty()) {
+                System.out.println("KO CO DU LIEU CHO KHOA NAY");
+            } else {
+                System.out.println("THONG KE: ");
+                result.forEach((k, v) -> {
+                    System.out.println(k + ":" + v);
+                });
+            }
+
+            System.out.println("THEM DOCTOR MOI");
+            Doctor newDoctor = Doctor
+                    .builder()
+                    .doctorId("DR.099")
+                    .name("AHIHI")
+                    .phone("09876543322")
+                    .departmentId("DP.099")
+                    .speciality("HIHI")
+                    .build();
+
+            doctorServiceImpl.addDoctor(newDoctor);
+            System.out.println("--- THEM DOCTOR THANH CONG! ---");
+            System.out.println(newDoctor);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
